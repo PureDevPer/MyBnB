466ca6d062bac3864c4a5feb518f4830e67d99e4
PureDevPer <wooseok.kim1207@gmail.com>
2019-10-20 17:24:48 -0400

    Added Birthday, Language, Currency, Superhost on admin

--- users/models.py
+++ users/models.py
@@ -16,8 +16,46 @@ class User(AbstractUser):
         (GENDER_OTHER, "Other"),
     )
 
+    LANGUAGE_CHINESE = "chinese"
+    LANGUAGE_ENGLISH = "english"
+    LANGUAGE_FRENCH = "french"
+    LANGUAGE_GERMAN = "german"
+    LANGUAGE_HINDI = "hindi"
+    LANGUAGE_KOREAN = "korean"
+    LANGUAGE_SPANISH = "spanish"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_CHINESE, "Chinese"),
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_FRENCH, "French"),
+        (LANGUAGE_GERMAN, "German"),
+        (LANGUAGE_HINDI, "Hindi"),
+        (LANGUAGE_KOREAN, "Korean"),
+        (LANGUAGE_SPANISH, "Spanish"),
+    )
+
+    CURRENCY_EUR = "eur"
+    CURRENCY_KRW = "krw"
+    CURRENCY_USD = "usd"
+    CURRENCY_YUAN = "yuan"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_EUR, "EURO"),
+        (CURRENCY_KRW, "KRW"),
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_YUAN, "YUAN"),
+    )
+
     avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(
         choices=GENDER_CHOICES, max_length=10, null=True, blank=True
     )
     bio = models.TextField(default="", blank=True)
+    birthday = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=10, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=10, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
